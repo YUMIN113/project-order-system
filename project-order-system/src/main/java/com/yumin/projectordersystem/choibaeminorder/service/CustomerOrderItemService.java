7207fb8ae655a7f697c5bc2e5cae62a115428cc4
yumin <cfy113@naver.com>
2022-12-30 00:14:13 +0900

    Update

--- project-order-system/src/main/java/com/yumin/projectordersystem/choibaeminorder/service/CustomerOrderItemService.java
+++ project-order-system/src/main/java/com/yumin/projectordersystem/choibaeminorder/service/CustomerOrderItemService.java
@@ -40,6 +40,7 @@ public class CustomerOrderItemService {
 
     }
 
+    // 주문 상품 정보 DB 저장
     public void saveCustomerOrderItemList(Long orderId, List<CustomerOrderItemRequestDto> customerOrderItemRequestDtoList) {
 
         List<CustomerOrderItem> saveCustomerOrderItem = customerOrderItemRequestDtoList
